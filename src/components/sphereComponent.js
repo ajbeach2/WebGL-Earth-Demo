e6f01846486fcf9eb8f8c77d659c64d5f7761698
Alex Beach <alex.beach@scientificgames.com>
2016-01-04 10:44:34 -0600

    more realistic textures

--- src/components/sphereComponent.js
+++ src/components/sphereComponent.js
@@ -2,11 +2,16 @@ import THREE from '../../lib/three.js'
 
 class SphereComponent extends THREE.Mesh {
   constructor(size) {
-    var texture = THREE.ImageUtils.loadTexture('textures/earthmap1k.jpg');
+    var texture = THREE.ImageUtils.loadTexture('textures/earthMap.jpg');
     var geometry = new THREE.SphereGeometry( 64, 64, 64 );
     var material =  new THREE.MeshPhongMaterial();
     material.map = texture;
   
+  	 material.bumpMap    = THREE.ImageUtils.loadTexture('textures/earthBump.jpg');
+	   material.specularMap    = THREE.ImageUtils.loadTexture('textures/earthSpec.png');
+	   material.bumpScale = 0.05
+	   material.specular  = new THREE.Color('grey');
+
     super( geometry, material );
     this.name = name || this.constructor.name;
   }
